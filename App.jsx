c09261e7dad7845e71c5936b17551d12e9e4877a
ChrisserDev <126911205+ChrisserDev@users.noreply.github.com>
2024-02-20 08:46:21 +0000

    Built the filtering section

--- App.jsx
+++ App.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 // import Dashboard from "./pages/Host/Dashboard"
 // import Income from "./pages/Host/Income"
 // import Reviews from "./pages/Host/Reviews"
-// import AuthRequired from "./components/AuthRequired"
-import HostLayout from "./components/HostLayout"
 import Login from "./pages/Login"
 import NotFound from "./pages/NotFound"
 import About from "./pages/About"
@@ -31,15 +29,12 @@ function App() {
           <Route path='booking/:id' element={<Booking />}/>
           <Route path="login" element={<Login />}/>
 {/* 
-          <Route element={<AuthRequired />}>
-            <Route path="host" element={<HostLayout />}>
               <Route index element={<Dashboard />} />
               <Route path="income" element={<Income />} />
               <Route path="reviews" element={<Reviews />} />
               </Route>
             </Route>
           </Route> */}
-
           <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
--- pages/Vehicles/AllVehicles.jsx
+++ pages/Vehicles/AllVehicles.jsx
@@ -3,141 +3,138 @@ import { MdPeopleAlt, MdLocalGasStation } from "react-icons/md";
 import { GiGearStickPattern } from "react-icons/gi";
 import { IoBag } from "react-icons/io5";
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function AllVehicles() {
-  const [displayVehicles, setDisplayVehicles] = useState([]);
-  const [types, setTypes] = useState([])
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useState(new URLSearchParams());
+    const [displayVehicles, setDisplayVehicles] = useState([]);
+    const [filteredVehicles, setFilteredVehicles] = useState([]);
+    const [formData, setFormData] = useState({
+        vehicleType: "",
+        gearType: "",
+        fuelType: "",
+        people: "",
+    });
 
-  useEffect(() => {
-    axios.get('http://localhost:4000/api/vehicles')
-      .then(response => {
-        console.log(response.data);
-        setDisplayVehicles(response.data);
-      })
-      .catch(error => console.log('Error fetching vehicles:', error));
-  }, []);
+    useEffect(() => {
+        axios.get(`http://localhost:4000/api/vehicles${searchParams.toString()}`)
+        .then(response => {
+                console.log(response.data);
+                setDisplayVehicles(response.data);
+            })
+            .catch(error => console.log('Error fetching vehicles:', error));
+    }, [searchParams]);
 
-//   useEffect(() => {
-//     axios.get('http://localhost:4000/types')
-//       .then(response => {
-//         console.log(response.data);
-//         setTypes(response.data);
-//       })
-//       .catch(error => console.log('Error fetching vehicles:', error));
-//   }, []);
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
 
-//   const getTypes = types.map(type => {
-//     return <p>{type}</p>
-//   })
+    const handleFilterChange = (key, value) => {
+        const sp = new URLSearchParams(searchParams);
+        if (value === null) {
+            sp.delete(key);
+        } else {
+            sp.set(key, value);
+        }
+    
+        // Set the searchParams directly, triggering the useEffect in the component
+        setSearchParams(sp);
+        
+        navigate(`/AllVehicles${sp.toString() ? `?${sp.toString()}` : ''}`);
+    };
 
-const capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
-  
+    const handleInputChange = (e) => {
+        const { id, value } = e.target;
 
-const vehicleElements = displayVehicles.map(vehicle => (
-    <div key={vehicle._id} className="vehicle-container">
-      <img src={vehicle.image} />
-      <div className="vehicle-section-one">
-        <h3>{vehicle.carName}</h3>
-        <strong>£{vehicle.price}<span>/day</span></strong>
-      </div>
-      <div className="vehicle-section-two">
-        <p><MdLocalGasStation />{capitalizeFirstLetter(vehicle.fuelType)}</p>
-        <p><GiGearStickPattern />{capitalizeFirstLetter(vehicle.gearType)}</p>
-        <p><MdPeopleAlt />{vehicle.people}</p>
-        <p><IoBag />{vehicle.bags}</p>
-       </div>
-        <details>{vehicle.features}</details>
-        {/* <li>{vehicle.features}</li> */}
-        <Link to={`/booking/${vehicle._id}`}>Select</Link>
-    </div>
-  ));
+        // Update form data with the new value, keeping the existing values
+        setFormData((prevFormData) => {
+            const updatedFormData = { ...prevFormData, [id]: value };
 
-  return (
-    <>
-    <form className='search-container'>
-        <div className='selected-choices'>
-            <strong><label htmlFor='vehicleType'>VEHICLE TYPE</label></strong>
-                <select id='vehicleType'>
-                    <option value="">----</option>
-                    <option value="car">Car</option>
-                    <option value="van">Van</option>
-                </select>
-        </div>
-        <div className='selected-choices'>
-            <strong><label htmlFor='transmission'>TRANSMISSION</label></strong>
-                <select id='transmission'>
-                    <option value="">----</option>
-                    <option value="manual">Manual</option>
-                    <option value="automatic">Automatic</option>
-                </select>
-        </div>
-        <div className='selected-choices'>
-            <strong><label htmlFor='fuelType'>FUEL TYPE</label></strong>
-                <select id='fuelType'>
-                    <option value="">----</option>
-                    <option value="petrol">Petrol</option>
-                    <option value="diesel">Diesel</option>
-                    <option value="electric">Electric</option>
-                </select>
-        </div>
-        <div className='selected-choices'>
-            <strong><label htmlFor='passengers'>NUMBER OF PASSENGERS</label></strong>
-                <select id='passengers'>
-                    <option value="">----</option>
-                    <option value="seven">7</option>
-                    <option value="five">5</option>
-                    <option value="four">4</option>
-                    <option value="three">3</option>
-                    <option value="two">2</option>
-                </select>
-        </div>
-    </form>
-    <div className='all-vehicles'>
-      {vehicleElements}
-    </div>
-  </>
-  );
+            // Update query parameters based on the updated form data
+            handleFilterChange(id, value);
+
+            const newFilteredVehicles = displayVehicles.filter((vehicle) => {
+                return (
+                    (updatedFormData.vehicleType === "" || vehicle.type === updatedFormData.vehicleType) &&
+                    (updatedFormData.fuelType === "" || vehicle.fuelType === updatedFormData.fuelType) &&
+                    (updatedFormData.gearType === "" || vehicle.gearType === updatedFormData.gearType) &&
+                    (updatedFormData.people === "" || vehicle.people === parseInt(updatedFormData.people, 10))
+                );
+            });
+
+            setFilteredVehicles(newFilteredVehicles);
+            return updatedFormData; // Return the updated form data
+        });
+    };
+
+    return (
+        <>
+            <form className='search-container'>
+                <div className='selected-choices'>
+                    <strong><label htmlFor='vehicleType'>VEHICLE TYPE</label></strong>
+                    <select id='vehicleType' value={formData.vehicleType} onChange={(e) => handleInputChange(e)}>
+                        <option value="">----</option>
+                        <option value="car">Car</option>
+                        <option value="van">Van</option>
+                    </select>
+                </div>
+                <div className='selected-choices'>
+                    <strong><label htmlFor='gearType'>TRANSMISSION</label></strong>
+                    <select id='gearType' value={formData.gearType} onChange={(e) => handleInputChange(e)}>
+                        <option value="">----</option>
+                        <option value="manual">Manual</option>
+                        <option value="automatic">Automatic</option>
+                    </select>
+                </div>
+                <div className='selected-choices'>
+                    <strong><label htmlFor='fuelType'>FUEL TYPE</label></strong>
+                    <select id='fuelType' value={formData.fuelType} onChange={(e) => handleInputChange(e)}>
+                        <option value="">----</option>
+                        <option value="petrol">Petrol</option>
+                        <option value="diesel">Diesel</option>
+                        <option value="electric">Electric</option>
+                    </select>
+                </div>
+                <div className='selected-choices'>
+                    <strong><label htmlFor='people'>NUMBER OF PASSENGERS</label></strong>
+                    <select id='people' value={formData.people} onChange={(e) => handleInputChange(e)}>
+                        <option value="">----</option>
+                        <option value="7">7</option>
+                        <option value="5">5</option>
+                        <option value="4">4</option>
+                        <option value="3">3</option>
+                        <option value="2">2</option>
+                    </select>
+                </div> 
+            </form>
+            <div className='all-vehicles'>
+                {(filteredVehicles.length > 0 ? filteredVehicles : displayVehicles).map(vehicle => (
+                    <div key={vehicle._id} className="vehicle-container">
+                        <img src={vehicle.image} alt={vehicle.carName} />
+                        <div className="vehicle-section-one">
+                            <h3>{vehicle.carName}</h3>
+                            <strong>£{vehicle.price}<span>/day</span></strong>
+                        </div>
+                        <div className="vehicle-section-two">
+                            <p><MdLocalGasStation />{capitalizeFirstLetter(vehicle.fuelType)}</p>
+                            <p><GiGearStickPattern />{capitalizeFirstLetter(vehicle.gearType)}</p>
+                            <p><MdPeopleAlt />{vehicle.people}</p>
+                            <p><IoBag />{vehicle.bags}</p>
+                        </div>
+                        <details>{vehicle.features}</details>
+                        <Link to={`/booking/${vehicle._id}`}>Select</Link>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export default function Cars() {
-    // const [searchParams, setSearchParams] = useSearchParams()
+/*
     // const [loading, setLoading] = React.useState(false)
     // const [error, setError] = React.useState(null)
-
-    // const typeFilter = searchParams.get("type")
-
-    // const displayedVans = typeFilter
-    //     ? vans.filter(van => van.type === typeFilter)
-    //     : vans
-
-
-    // function handleFilterChange(key, value) {
-    //     setSearchParams(prevParams => {
-    //         if (value === null) {
-    //             prevParams.delete(key)
-    //         } else {
-    //             prevParams.set(key, value)
-    //         }
-    //         return prevParams
-    //     })
-    // }
 
     // if (loading) {
     //     return <h1>Loading...</h1>
@@ -146,45 +143,4 @@ const vehicleElements = displayVehicles.map(vehicle => (
     // if (error) {
     //     return <h1>There was an error: {error.message}</h1>
     // }
-
-    // return (
-    //     <div className="van-list-container">
-    //         <h1>Explore our car options</h1>
-            /* <div className="van-list-filter-buttons">
-                <button
-                    onClick={() => handleFilterChange("type", "simple")}
-                    className={
-                        `van-type simple 
-                        ${typeFilter === "simple" ? "selected" : ""}`
-                    }
-                >Simple</button>
-                <button
-                    onClick={() => handleFilterChange("type", "luxury")}
-                    className={
-                        `van-type luxury 
-                        ${typeFilter === "luxury" ? "selected" : ""}`
-                    }
-                >Luxury</button>
-                <button
-                    onClick={() => handleFilterChange("type", "rugged")}
-                    className={
-                        `van-type rugged 
-                        ${typeFilter === "rugged" ? "selected" : ""}`
-                    }
-                >Rugged</button>
-
-                {typeFilter ? (
-                    <button
-                        onClick={() => handleFilterChange("type", null)}
-                        className="van-type clear-filters"
-                    >Clear filter</button>
-                ) : null}
-
-            </div>
-            <div className="van-list">
-                {vanElements}
-            </div> */
-//         </div>
-//     )
-// }
-
+*/
