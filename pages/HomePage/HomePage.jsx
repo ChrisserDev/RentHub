f3106fb825e92e2aac63c856a95c8807433fe85d
ChrisserDev <126911205+ChrisserDev@users.noreply.github.com>
2024-03-05 09:05:11 +0000

    Fixed the space fot larger screens

--- pages/HomePage/HomePage.jsx
+++ pages/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     return (
         <div className="home-container">
             <h1>You got the travel plans, we got the travel vehicle.</h1>
-            <p>Add adventure to your life by joining the <strong>#RentAVehicleHub</strong> movement. Rent the perfect vehicle to make your perfect road trip.</p>
+            <p>Add adventure to your life by joining the <strong>#RentHub</strong> movement. Rent the perfect vehicle to make your perfect road trip.</p>
             <div className="about-page-cta">
                 <Link className="link-button" to="/AllVehicles">Explore our vehicles</Link>
             </div>
